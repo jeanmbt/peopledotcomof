c7309198aba7ae6fb80e623e7e9ff0bcf31c38b5
Jean Michel Battirola <jeanbattirola@gmail.com>
2022-04-06 16:00:57 +0200

    TODO: fix emotion/mui issues, query on /coaches, fix server

--- styles/theme/lightThemeOptions.ts
+++ styles/theme/lightThemeOptions.ts
@@ -1,12 +1,13 @@
-import { ThemeOptions } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 
-const lightThemeOptions: ThemeOptions = {
+const theme = createTheme({
   palette: {
     mode: 'light',
     primary: { main: "#481593" },
     secondary: { main: "#ff0000" },
     contrastThreshold: 2,
   },
-};
+});
 
-export default lightThemeOptions;
+export  default theme
+
